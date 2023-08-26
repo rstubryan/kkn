0f40249f3837e1e8ddb98c573c32a1b665aaec77
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-08-26 20:07:38 +0700

    about diupdate dan css responsive

--- src/about.js
+++ src/about.js
@@ -12,9 +12,9 @@ function abt() {
 
     <div class="container mx-auto rounded-3xl bg-[#10482A] py-4 w-[1100px]">
       <div class="w-[1000px] container mx-auto">
-      <p class="mb-3 text-lg text-white md:text-xl text-center">KKN di Desa Wangunsari</p>
-      <p class="text-white">Desa Wangunsari merupakan</p>
-      <p class="mb-3 text-right text-white">Desa Wangunsari merupakan</p>
+      <p class="mb-3 text-lg text-white md:text-xl text-center font-bold">Desa <span class="text-[#F4D9A6]">Wangunsari</span></p>
+      <p class="text-white md:text-base text-xs py-2 px-4"><span class="font-extrabold text-[#F4D9A6]">Wangunsari</span> adalah desa di kecamatan Cisolok, Sukabumi, Jawa Barat, Indonesia. Desa ini merupakan hasil pemekaran wilayah desa Cisolok sesuai dengan Peraturan Daerah Kabupaten Sukabumi Nomor 16 Tahun 2012.</p>
+      <p class="mb-3 text-right text-white md:text-base text-xs py-2 px-4">Desa Wangunsari memiliki luas 3,70 km² dengan jumlah penduduk mencapai 1.113 jiwa (2012). Desa <span class="font-extrabold text-[#F4D9A6]">Wangunsari</span> memiliki destinasi pariwisata diantaranya Geyser Cisolok dan Curug Cierang.</p>
       </div>
     </div>
   </section>
